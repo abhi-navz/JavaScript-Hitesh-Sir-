6ce543ced07e092cea50d58098373afcb35a2ba4
abhi-navz <abhinavg01441@gmail.com>
2023-06-17 22:46:53 +0530

    some styling

--- Project/Digital Clock/script.js
+++ Project/Digital Clock/script.js
@@ -1,6 +1,10 @@
 const clock = document.querySelector("#clock");
+const time = document.querySelector("#time");
+const datee = document.querySelector("#date");
 
 setInterval(function () {
   let date = new Date();
-  clock.innerHTML = date.toLocaleTimeString();
+  time.innerHTML = date.toLocaleTimeString();
+  datee.innerHTML = date.toDateString();
+  
 }, 1000);
